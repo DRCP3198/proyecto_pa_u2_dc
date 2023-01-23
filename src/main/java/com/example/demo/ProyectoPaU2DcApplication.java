629bdf7cc5c4b1c7dce6c35e6aa9b6e5062a788f
Dillan Renato Coloma Pozo <renatocoloma2010@gmail.com>
2023-01-22 21:43:05 -0500

    Tarea 8 Mapeo de relaciones OneToOne

--- src/main/java/com/example/demo/ProyectoPaU2DcApplication.java
+++ src/main/java/com/example/demo/ProyectoPaU2DcApplication.java
@@ -13,19 +13,24 @@ import com.example.demo.uce.modelo.Empleado;
 import com.example.demo.uce.modelo.Estudiante;
 import com.example.demo.uce.modelo.Profesor;
 import com.example.demo.uce.service.ICiudadanoService;
+import com.example.demo.uce.service.IEmpleadoService;
 import com.example.demo.uce.service.IEstudianteService;
 import com.example.demo.uce.service.IProfesorService;
 
 @SpringBootApplication
 public class ProyectoPaU2DcApplication implements CommandLineRunner {
 
-	@Autowired
+	/*@Autowired
 	private IEstudianteService estudianteService;
 	
 	@Autowired
-	private IProfesorService iProfesorService;
+	private IProfesorService iProfesorService;*/
+	
 	@Autowired
 	private ICiudadanoService ciudadanoService;
+	
+	@Autowired
+	private IEmpleadoService empleadoService;
 	
 	
 	public static void main(String[] args) {
@@ -35,47 +40,21 @@ public class ProyectoPaU2DcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Estudiante estu = new Estudiante();
-		//estu.setId(2);
-		estu.setNombre("RENATO");
-		estu.setApellido("POZO");
-		estu.setCedula("1751457167");
-		estu.setCiudad("Quito");
-		estu.setGenero("M");
 		
-	    this.estudianteService.agregar(estu);
-		/*this.estudianteService.borrar(1);
-		this.estudianteService.borrar(3);
-		this.estudianteService.borrar(4);*/
-	  //System.out.println(this.estudianteService.encontrar(4));*/
-		
-		//Profesor prof = this.iProfesorService.encontrar(1);
-		/*Profesor prof = new Profesor();
-		prof.setNombre("Dillan");
-		prof.setApellido("Coloma");
-		prof.setMateria("Fisica");
-		prof.setCedula("1725247645");
-		prof.setNivelEstudio("Tercer Nivel");
-		prof.setTelefono("0979200332");
-		
-		//this.iProfesorService.agregar(prof);
-		prof.setId(4);
-		prof.setApellido("Pozo");
-		prof.setNivelEstudio("CuartoNivel");
-		
-		this.iProfesorService.modificar(prof);*/
 		
 	    Ciudadano ciu = new Ciudadano();
 	    ciu.setNombre("Dillan");
 	    ciu.setApellido("Coloma");
-	    
+	    this.ciudadanoService.guardar(ciu);
 	    Empleado empleado = new Empleado();
 	    empleado.setSalario(new BigDecimal(20));
 	    empleado.setIngreso(LocalDateTime.now());
 	    empleado.setCiudadano(ciu);
 	    ciu.setEmpleado(empleado);
 	    
-	    //this.ciudadanoService.guardar(ciu);
+	    this.empleadoService.agregar(empleado);
+	 
+	   
 	    
 		
 		
--- src/main/java/com/example/demo/uce/service/ICiudadanoService.java
+++ src/main/java/com/example/demo/uce/service/ICiudadanoService.java
@@ -5,5 +5,8 @@ import com.example.demo.uce.modelo.Ciudadano;
 public interface ICiudadanoService {
 	
 	public void guardar(Ciudadano ciudadano);
+	public void modificar(Ciudadano ciudadano);
+	public Ciudadano encontrar(Integer id);
+	public void borrar(Integer id);
 
 }
